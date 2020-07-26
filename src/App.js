7324dc1fa66aba5f5f0ddfb308d5dffda4be3839
prass07 <prasup07@gmail.com>
2020-07-26 14:06:38 +0530

    finished the homepage component by sub dividing directory menu and menu items into there own component folder and also finalizng styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component.jsx';
+import HomePage from './pages/hompepage/homepage.component.jsx';
 
 function App() {
   return (
---
+++ src/pages/hompepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
+
+const HomePage = () => {
+    return(
+        <div className = 'homepage'>
+            <DirectoryMenu />
+        </div>
+    );
+}
+
+export default HomePage;
