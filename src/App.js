fda1be7ca190cd0e3c7cbbe13b6b88b05056f1d2
prass07 <prasup07@gmail.com>
2020-07-27 11:55:43 +0530

    used react router dom and used it to route hats page from the homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/hompepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <h1>Hats</h1>
+)
+
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path ='/' component = {HomePage} />
+        <Route path = '/hats' component = {HatsPage} />
+      </Switch>
     </div>
   );
 }
