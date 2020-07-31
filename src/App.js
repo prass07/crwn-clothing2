d58e97a4dccdccddc277869d55cab3c89f8b7308
prass07 <prasup07@gmail.com>
2020-07-31 13:19:58 +0530

    created signup component,added the user from signup component to the database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/hompepage/homepage.component.jsx';
 import ShopPage from './pages/shoppage/shoppage.component';
 import SigninAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -21,10 +21,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot =>{
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state)
+        });
+      }
+      else{
+        this.setState({ currentUser: userAuth});
+      }
     });
   }
 
