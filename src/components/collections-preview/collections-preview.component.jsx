7a304d8849883b9ebb529a83e23a7ebdbd7a6ff9
prass07 <prasup07@gmail.com>
2020-08-04 16:48:45 +0530

    moved our shop data and directory to there own reducer, created respective selectors

--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -10,8 +10,8 @@ const CollectionPreview = ({title, items}) =>{
             <h1 className = 'title'>{title.toUpperCase()}</h1>
             <div className = 'preview'>
                 {items
-                    .filter((item,idx)=> idx < 4 )
-                    .map((item) =>(
+                    .filter((item,idx)=> idx < 5 )
+                    .map(item =>(
                         <CollectionItem key = {item.id} item = {item} />
                     ))
                 }
--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shopdata';
-import CollectionPreview from '../../components/collections-preview/collections-preview.component';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render(){
-        const { collections } = this.state;
+const ShopPage = ({ match }) => {
+        
         return(
             <div className = 'shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionsProps}) => (
-                        <CollectionPreview key = {id} {...otherCollectionsProps} />
-                    ))
-                 }
+                <Route exact path ={`${match.path}`} component = { CollectionOverview } />
+                <Route path = {`${match.path}/:collectionId`} component = { CollectionPage } />
             </div>
         );
-    }
 }
+
 
 export default ShopPage;
